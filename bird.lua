32c238f7646dc38637dd1c711fc8f81deb1a018b
Yash <ypt7895@gmail.com>
2023-07-10 00:31:24 +0530

    added basic bird functionality

---
+++ bird.lua
@@ -0,0 +1,25 @@
+Bird = Entity:extend()
+
+function Bird:new(x,y,img)
+    Bird.super.new(self,x,y,img)
+    self.width = self.img:getWidth()
+    self.height = self.img:getHeight()
+    self.vel = 0
+    self.grav = 45
+end
+
+function Bird:update(dt)
+    self.vel = self.vel + self.grav*dt
+    if love.keyboard.isDown("up") and self.y>0 then
+        self.vel = -8
+    end
+    if self.vel>55 then
+        self.vel = 55
+    end
+    self.y = self.y + self.vel
+    if self.y>540 then
+        self.y = 540
+    elseif self.y<0 then
+        self.y = 0
+    end
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+function love.load()
+    love.window.setMode(480,600)
+    Object = require "classic"
+    require "entity"
+    require "bird"
+    BirdImg = love.graphics.newImage("/assets/images/redbird-midflap.png")
+    BirdPlayer = Bird(80,90,BirdImg)
+end
+
 function love.draw()
-    love.graphics.draw(love.graphics.newText(love.graphics.getFont(),"Hello"),3,7)
+    love.graphics.setBackgroundColor(0,197,207,1)
+    BirdPlayer:draw()
+end
+
+function love.update(dt)
+    BirdPlayer:update(dt)
 end
