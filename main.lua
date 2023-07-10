b3edca3cf1c92ee95c996e371b48803b51f31a05
Yash <ypt7895@gmail.com>
2023-07-10 20:16:30 +0530

    testing obstacles

--- main.lua
+++ main.lua
@@ -1,21 +1,21 @@
 function love.load()
-    love.window.setMode(480,480)
+    love.window.setMode(640,480)
     Object = require "classic"
     require "entity"
     require "bird"
     require "obstacles"
     BirdImg = love.graphics.newImage("/assets/images/redbird-midflap.png")
     ObstacleImg = love.graphics.newImage("/assets/images/pipe-green.png")
-    BirdPlayer = Bird(80,90,BirdImg)
-    ObstacleList = {Obstacle(120, ObstacleImg), Obstacle(210, ObstacleImg), Obstacle(300, ObstacleImg), Obstacle(390, ObstacleImg), Obstacle(480, ObstacleImg)}
+    BirdPlayer = Bird(75,240,BirdImg)
+    ObstacleList = {Obstacle(120), Obstacle(240), Obstacle(360), Obstacle(480), Obstacle(600), Obstacle(720)}
 end
 
 function love.draw()
     love.graphics.setBackgroundColor(0,0.65,0.93,1)
-    BirdPlayer:draw()
     for i, obs in ipairs(ObstacleList) do
         obs:draw()
     end
+    BirdPlayer:draw()
 end
 
 function love.update(dt)
--- obstacles.lua
+++ obstacles.lua
@@ -1,15 +1,23 @@
-Obstacle = Entity:extend()
+Obstacle = Object.extend(Object)
 
-function Obstacle:new(x,img)
-    y = love.math.random(50,250)
-    Obstacle.super.new(self,x,y,img)
-    self.width = self.img:getWidth()
-    self.height = self.img:getHeight()
+function Obstacle:new(x)
+    self.x = x
+    self.y = love.math.random(120,290)
+    self.width = 55
+    self.space = 110
+end
+
+function Obstacle:draw()
+    love.graphics.setColor(0.1,0.9,0.1)
+    love.graphics.rectangle("fill",self.x,0,self.width,self.y)
+    love.graphics.rectangle("fill",self.x,self.y+self.space,self.width,love.graphics.getHeight()-self.y-self.space)
+    love.graphics.setColor(1,1,1)
 end
 
 function Obstacle:update(dt)
     self.x = self.x - 50*dt
     if self.x<(-1*self.width) then
-        self.x = love.graphics.getWidth()
+        self.x = 720
+        self.y = love.math.random(50,390)
     end
 end
