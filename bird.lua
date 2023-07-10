dcefbcdcbb005ea6d4b290a555188c46ebe116c6
Yash <ypt7895@gmail.com>
2023-07-10 20:16:08 +0530

    changed bird mechanics

--- bird.lua
+++ bird.lua
@@ -5,20 +5,20 @@ function Bird:new(x,y,img)
     self.width = self.img:getWidth()
     self.height = self.img:getHeight()
     self.vel = 0
-    self.grav = 45
+    self.grav = 15
 end
 
 function Bird:update(dt)
     self.vel = self.vel + self.grav*dt
     if love.keyboard.isDown("up") and self.y>0 then
-        self.vel = -8
+        self.vel = -3
     end
-    if self.vel>55 then
-        self.vel = 55
+    if self.vel>25 then
+        self.vel = 25
     end
     self.y = self.y + self.vel
-    if self.y>540 then
-        self.y = 540
+    if self.y>440 then
+        self.y = 440
     elseif self.y<0 then
         self.y = 0
     end
