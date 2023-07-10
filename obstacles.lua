bcc7ba005f11edf5a22cc5a2f5ae9e49229b1800
Yash <ypt7895@gmail.com>
2023-07-10 22:31:20 +0530

    minimal gameplay enhancements

--- obstacles.lua
+++ obstacles.lua
@@ -2,8 +2,8 @@ Obstacle = Object.extend(Object)
 
 function Obstacle:new(x)
     self.x = x
-    self.y = love.math.random(120,290)
-    self.width = 55
+    self.y = love.math.random(100,290)
+    self.width = 60
     self.space = 110
 end
 
@@ -17,7 +17,7 @@ end
 function Obstacle:update(dt)
     self.x = self.x - 50*dt
     if self.x<(-1*self.width) then
-        self.x = 720
-        self.y = love.math.random(50,390)
+        self.x = 660
+        self.y = love.math.random(100,290)
     end
 end
