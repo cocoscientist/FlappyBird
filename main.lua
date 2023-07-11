82547fa48f95d9e5d9c994b150eb4df3bc8d80b5
Yash <ypt7895@gmail.com>
2023-07-11 15:01:27 +0530

    restart condition

--- main.lua
+++ main.lua
@@ -7,7 +7,6 @@ function love.load()
     Score = 0
     GameOver = false
     BirdImg = love.graphics.newImage("/assets/images/redbird-midflap.png")
-    ObstacleImg = love.graphics.newImage("/assets/images/pipe-green.png")
     BirdPlayer = Bird(75,240,BirdImg)
     ObstacleList = {Obstacle(180), Obstacle(300), Obstacle(420), Obstacle(540), Obstacle(660), Obstacle(780)}
 end
@@ -17,18 +16,34 @@ function love.draw()
     for i, obs in ipairs(ObstacleList) do
         obs:draw()
     end
+    love.graphics.setColor(0.478,0.478,0.2)
+    love.graphics.rectangle("fill",0,440,640,40)
+    love.graphics.setColor(1,1,1)
     BirdPlayer:draw()
     love.graphics.draw(love.graphics.newText(love.graphics.getFont(),"Score: "..tostring(Score)),3,7)
     if GameOver then
-        love.graphics.setColor(0.3,0.1,0.5)
-        love.graphics.rectangle("fill",240,240,240,240)
+        love.graphics.setColor(0.1,0.3,0.5)
+        love.graphics.rectangle("fill",240,180,180,100)
         love.graphics.setColor(1,1,1)
-        love.graphics.draw(love.graphics.newText(love.graphics.getFont(),"Game Over, Score: "..tostring(Score)),360,360)
+        love.graphics.draw(love.graphics.newText(love.graphics.getFont(),"Game Over, Score: "..tostring(Score)),265,200)
+        love.graphics.draw(love.graphics.newText(love.graphics.getFont(),"Press Up to Restart"),265,220)
+    end
+end
+
+function love.keypressed(key)
+    if GameOver and key=="up" then
+        Score = 0
+        GameOver = false
+        BirdPlayer = Bird(75,240,BirdImg)
+        ObstacleList = {Obstacle(180), Obstacle(300), Obstacle(420), Obstacle(540), Obstacle(660), Obstacle(780)}
     end
 end
 
 function love.update(dt)
     BirdPlayer:update(dt)
+    if BirdPlayer.y>439 then
+        GameOver = true
+    end
     if not GameOver then
         for i, obs in ipairs(ObstacleList) do
             obs:update(dt)
